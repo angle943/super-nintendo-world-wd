9f7d4f3b61804f1605b811de2fc1451b3751e17d
Kevin Walker <k.walker@sae-sf.net>
2023-05-22 01:40:46 +0200

    Build ride and experiences section

---
+++ rides-and-experiences/data.js
@@ -0,0 +1,50 @@
+let cardData = [
+  {
+    title: "Show 2 Name",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's took a galley of type and scrambled it",
+    img: {
+      src: "/assets/bowser-castle.jpg",
+      alt: "",
+    },
+    category: "shows",
+    misc: {},
+  },
+  {
+    title: "Resorts 2 Name",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's took a galley of type and scrambled it",
+    img: {
+      src: "/assets/bowser-castle.jpg",
+      alt: "",
+    },
+
+    category: "resorts",
+    misc: {},
+  },
+  {
+    title: "Dining 2 Name",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's took a galley of type and scrambled it",
+    img: {
+      src: "/assets/bowser-castle.jpg",
+      alt: "",
+    },
+
+    category: "dining",
+    misc: {},
+  },
+  {
+    title: "Attractions 2 Name",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's took a galley of type and scrambled it",
+    img: {
+      src: "/assets/bowser-castle.jpg",
+      alt: "",
+    },
+    category: "attractions",
+    misc: {},
+  },
+];
+
+export default cardData;
