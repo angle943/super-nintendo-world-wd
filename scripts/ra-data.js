5bc1ab86bf83d4e79e738a15e08efaa3f0478c19
angle943 <justinkim943@gmail.com>
2023-06-25 17:06:58 -0700

    add more text

--- scripts/ra-data.js
+++ scripts/ra-data.js
@@ -5,7 +5,7 @@ const raData = [
     {
         title: "Bowser's BBQ Bonanza",
         description: "Feast on fiery, savory barbecue favorites overseen by Bowser himself, offering a villainously delicious dining experience.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/bowsers-bbq.jpeg",
             alt: "Attraction image",
@@ -16,7 +16,7 @@ const raData = [
     {
         title: "Bowser's Comedy Castle",
         description: "Laugh out loud at Bowser's hilarious stand-up comedy routine, where the villainous Koopa King showcases his unexpectedly comedic side.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/splash.jpg",
             alt: "Attraction image",
@@ -26,7 +26,7 @@ const raData = [
     {
         title: "Brinstar",
         description: "Experience the life as Samus in this terrifying world of Brinstar",
-        more: "TO BE COMPLETED...",
+        more: "Must be 10 years or older",
         img: {
             src: "./static/rides-and-attractions/brinstar.jpg",
             alt: "Attraction image",
@@ -36,7 +36,7 @@ const raData = [
     {
         title: "Donkey Kong's Barrel Blast",
         description: "Swing through a thrilling barrel coaster ride with Donkey Kong, navigating obstacles and daring jumps.",
-        more: "TO BE COMPLETED...",
+        more: "Must be 6 years or older",
         img: {
             src: "./static/rides-and-attractions/Donkey-Kong.jpeg",
             alt: "Attraction image",
@@ -46,7 +46,7 @@ const raData = [
     {
         title: "Donkey Kong's Drumbeat Rhythm Show",
         description: "Feel the rhythm and get your groove on as Donkey Kong and his crew take center stage, delivering a lively and entertaining drumbeat performance that will have you dancing in your seat.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/kong-show.jpeg",
             alt: "Attraction image",
@@ -56,7 +56,7 @@ const raData = [
     {
         title: "Donkey Kong's Jungle Jam",
         description: "Get ready for a rhythm-infused extravaganza as Donkey Kong and his crew showcase their groovy moves in an energetic dance and percussion performance, creating an unforgettable spectacle of beats and entertainment.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/kong-show2.jpeg",
             alt: "Attraction image",
@@ -67,7 +67,7 @@ const raData = [
         title: "Donkey Kong's Jungle Hideaway",
         description:
           "Escape to the untamed jungles of Donkey Kong's tropical hideaway, where you can swing on vines, discover hidden treasures, and experience a wild and thrilling retreat.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages. Adult supervision required.",
         img: {
             src: "./static/rides-and-attractions/Kong-resort.jpeg",
             alt: "Attraction image",
@@ -78,7 +78,7 @@ const raData = [
         title: "Hyrule Highlands Resort",
         description:
           "Immerse yourself in the enchanting world of Hyrule at this majestic resort nestled amidst lush greenery and towering mountains, offering a serene escape with a touch of legendary adventure.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages. Adult supervision required.",
         img: {
             src: "./static/rides-and-attractions/hyrule-resort.jpeg",
             alt: "Attraction image",
@@ -89,7 +89,7 @@ const raData = [
         title: "Kirby's Dream Buffet",
         description:
           "Step into a magical confectionery world with Kirby, where you can indulge in a variety of colorful and sweet delights.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/kirby-buffet.jpeg",
             alt: "Attraction image",
@@ -100,7 +100,7 @@ const raData = [
     {
         title: "Kirby's Dreamland Dance Party",
         description: "Join Kirby and friends in an energetic dance party, grooving to catchy tunes and showcasing your best moves in a lively and colorful atmosphere.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/kirby-show.jpeg",
             alt: "Attraction image",
@@ -110,7 +110,7 @@ const raData = [
     {
         title: "Kirby's Flying Spectacular",
         description: "Immerse yourself in a whimsical journey through Dreamland with Kirby and his friends in a magical stage show filled with acrobatics, music, and captivating performances, leaving the audience enchanted and filled with joy.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/kirby-flying.jpeg",
             alt: "Attraction image",
@@ -121,7 +121,7 @@ const raData = [
         title: "Koopa Troopa's Beachside Retreat",
         description:
           "Relax and enjoy a playful beachside getaway at Koopa Troopa's tropical resort, complete with thrilling activities and surprises.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages. Adult supervision required.",
         img: {
             src: "./static/rides-and-attractions/koopa-resort.jpeg",
             alt: "Attraction image",
@@ -131,7 +131,7 @@ const raData = [
     {
         title: "Koopa's Crazy Kart Rally",
         description: "Join the Koopa Troop in a fast-paced go-kart race against Mario and friends, complete with power-ups and exciting shortcuts.",
-        more: "TO BE COMPLETED...",
+        more: "Must be 6 years or older",
         img: {
             src: "./static/rides-and-attractions/Koopas-kart.jpeg",
             alt: "Attraction image",
@@ -142,7 +142,7 @@ const raData = [
         title: "Luigi's Burgers",
         description:
           "Savor authentic Burgers prepared by Luigi, showcasing his culinary skills and love for hearty comfort food.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/Luigis-burgers.jpeg",
             alt: "Attraction image",
@@ -154,7 +154,7 @@ const raData = [
         title: "Mama Mia Bakery",
         description:
           "You deserve some cake from Princess Peach after all the adventuring! Come and get some cake and other desserts that will make you say Mama Mia!",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/mama-mia-bakery.jpeg",
             alt: "Attraction image",
@@ -165,7 +165,7 @@ const raData = [
     {
         title: "Mario and Friends' Super Spectacular Showdown",
         description: "Get ready for an electrifying stage show as Mario, Luigi, Princess Peach, and their pals display their extraordinary talents in a high-energy performance filled with stunts, music, and excitement.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/mario-show.jpeg",
             alt: "Attraction image",
@@ -176,7 +176,7 @@ const raData = [
         title: "Mario's Pizza Paradise",
         description:
           "Enjoy a slice of piping-hot, handcrafted pizza made by none other than Mario himself, featuring a variety of Mushroom Kingdom-inspired toppings.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/Mario-pizza.jpeg",
             alt: "Attraction image",
@@ -188,7 +188,7 @@ const raData = [
         title: "Mushroom Kingdom Inn",
         description:
           "Experience a cozy and enchanting stay at the Mushroom Kingdom Inn, where whimsical decor and warm hospitality await guests of all ages.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages. Adult supervision required.",
         img: {
             src: "./static/rides-and-attractions/mushroom-resort.jpeg",
             alt: "Attraction image",
@@ -198,7 +198,7 @@ const raData = [
     {
         title: "Pokémon Battle Extravaganza",
         description: "Witness an epic display of Pokémon battles featuring trainers and their Pokémon engaging in electrifying duels, showcasing their skills and teamwork.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/pokemon-show.jpeg",
             alt: "Attraction image",
@@ -208,7 +208,7 @@ const raData = [
     {
         title: "Princess Peach's Castle Escape",
         description: "Help Princess Peach escape from Bowser's clutches in an interactive and immersive castle-themed experience.",
-        more: "TO BE COMPLETED...",
+        more: "Must be 6 years or older",
         img: {
             src: "./static/rides-and-attractions/Princess-Peach-Castle.jpeg",
             alt: "Attraction image",
@@ -218,7 +218,7 @@ const raData = [
     {
         title: "Star Power Roller Coaster",
         description: "Feel the rush as you ride a high-speed roller coaster through a galaxy filled with sparkling stars and cosmic wonders.",
-        more: "TO BE COMPLETED...",
+        more: "Must be 6 years or older",
         img: {
             src: "./static/rides-and-attractions/star-power-rollercoaster.jpeg",
             alt: "Attraction image",
@@ -229,7 +229,7 @@ const raData = [
         title: "Star Road Lodge",
         description:
           "Journey to the cosmos and unwind at Star Road Lodge, a celestial retreat where you can stargaze from your balcony and enjoy luxurious amenities inspired by the wonders of the universe.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages. Adult supervision required.",
         img: {
             src: "./static/rides-and-attractions/Star-resort.jpeg",
             alt: "Attraction image",
@@ -239,7 +239,7 @@ const raData = [
     {
         title: "The Legend of Zelda",
         description: "Ocarina of Time Live: Immerse yourself in the enchanting melodies of Hyrule through a captivating live orchestral performance, showcasing the timeless music of The Legend of Zelda: Ocarina of Time.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/zelda-show.jpeg",
             alt: "Attraction image",
@@ -250,7 +250,7 @@ const raData = [
         title: "Toadstool Terrace Retreat",
         description:
           "Discover tranquility and charm at the Toadstool Terrace Retreat, nestled in the heart of Super Nintendo World. Enjoy cozy accommodations, lush gardens, and delightful amenities, providing a peaceful escape for adventurers of all ages.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages. Adult supervision required.",
         img: {
             src: "./static/rides-and-attractions/toadstool-resort.jpeg",
             alt: "Attraction image",
@@ -261,7 +261,7 @@ const raData = [
         title: "Toad's Mushroom Munchies",
         description:
           "Delight in a range of delectable Mushroom Kingdom-inspired treats and snacks at Toad's charming food kiosk.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/tod-munchies.jpeg",
             alt: "Attraction image",
@@ -272,7 +272,7 @@ const raData = [
     {
         title: "Yoshi's Epic Egg Hunt",
         description: "Embark on an egg-citing adventure with Yoshi, searching for hidden eggs and encountering delightful surprises along the way.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/Yoshi.jpeg",
             alt: "Attraction image",
@@ -283,7 +283,7 @@ const raData = [
         title: "Yoshi's Island Oasis",
         description:
           "Indulge in a tranquil and tropical paradise at Yoshi's Island Oasis, where vibrant flora, serene waterfalls, and friendly Yoshi companions create a peaceful and rejuvenating getaway.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages. Adult supervision required.",
         img: {
             src: "./static/rides-and-attractions/yoshi-resort.jpeg",
             alt: "Attraction image",
@@ -294,7 +294,7 @@ const raData = [
         title: "Yoshi's Snack-a-Saurus Cafe",
         description:
             "Refuel with tasty snacks and refreshing drinks at Yoshi's whimsical and vibrant cafe, perfect for a quick bite.",
-        more: "TO BE COMPLETED...",
+        more: "Suitable for all ages",
         img: {
             src: "./static/rides-and-attractions/Yoshi-snacks.jpeg",
             alt: "Attraction image",
@@ -306,7 +306,7 @@ const raData = [
         title: "World 1-1",
         description:
           "Start your new adventure as Mario in this world 1-1. Don't worry about losing your life, this is the first level!",
-        more: "TO BE COMPLETED...",
+        more: "Must be 6 years or older",
         img: {
             src: "./static/rides-and-attractions/world-1-1.png",
             alt: "Attraction image",
